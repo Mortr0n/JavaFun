46b2019ee850ebee9e874aa35b8c4d4324bfd567
Chris <cmsl4y3r@gmail.com>
2021-08-27 21:00:25 -0600

    initial commit

---
+++ JavaFirst.java
@@ -0,0 +1,8 @@
+public class JavaFirst {
+    public static void main(String[] args){
+        System.out.println("My name is Chris Morton");
+        System.out.println("I am 41 years old");
+        System.out.println("My hometown is Newnan, GA");
+        System.out.println("I currently live in Missoula, MT");
+    }
+}
